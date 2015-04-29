643151a4be4719380f7f86825b6727d25008fd2d
Tyler Wiegand <tyler.wiegand@me.com>
2015-04-29 16:48:53 -0600

    Added Admin section to personas.php. Fixed layout of html code. https://app.asana.com/0/32737638783261/32737638783270

--- specification/personas.php
+++ specification/personas.php
@@ -7,17 +7,64 @@
 	</head>
 
 	<body>
-		<h2>The Individual</h2>
-		<p> <strong>Name:</strong> Ted Ernest Bayer<br />
+		<h1>The Individual</h1>
+		<p>
+			<strong>Name:</strong> Ted Ernest Bayer<br />
+
 			<strong>Age:</strong> 32...ish<br />
-			<strong>Profession:</strong> Mr. Bayer is a paper salesman for a small company, often bored by his job and looking for a little clean excitement to add to his often cluttered desk.<br/>
-			<strong>Technology:</strong> Works from a low-end eMachine running Windows Vista, the same model as everyone else in his office. At home he has a budget PC build, accessing our site through Firefox.
-			He doesn't use his Android phone much for accessing the web, due to data caps.<br />
-			<strong> Attitudes and Behaviours:</strong> Ted is an opinionated single guy with a little disposable income who enjoys debating with his colleagues almost as much as he likes to keep up with the Kardashians.
-			Both a political and social media information hound, he spends his time at work arguing with people online and taking BuzzFeed quizzes while attempting to engage his coworkers in friendly banter.<br />
-			<strong> Frustrations and Needs:</strong> Ted is sick of people walking by his desk and not having anything to say to him; he hates having to initiate conversations...
-			So Ted would like to put figurines, nay, <em>controversial</em> conversation starters on his desk...Not only for himself, but for the excitement of the whole office.<br />
-			<strong>Goals:</strong> He needs to find a figurine that represents his views. He needs to find a figurine just right for that cute receptionist who seems to think he's funny.
-			He needs whimsical gifts for his family and friends, and he wants them all in one place.</p>
+
+			<strong>Profession:</strong> Mr. Bayer is a paper salesman for a small company,
+			often bored by his job and looking for a little clean excitement to add to his
+			often cluttered desk.<br/>
+
+			<strong>Technology:</strong> Works from a low-end eMachine running Windows Vista,
+			the same model as everyone else in his office. At home he has a budget PC build,
+			accessing our site through Firefox. He doesn't use his Android phone much for
+			accessing the web, due to data caps.<br />
+
+			<strong> Attitudes and Behaviours:</strong> Ted is an opinionated single guy with
+			a little disposable income who enjoys debating with his colleagues almost as much
+			as he likes to keep up with the Kardashians. Both a political and social media
+			information hound, he spends his time at work arguing with people online and taking
+			BuzzFeed quizzes while attempting to engage his coworkers in friendly banter.<br />
+
+			<strong> Frustrations and Needs:</strong> Ted is sick of people walking by his desk
+			and not having anything to say to him; he hates having to initiate conversations...
+			So Ted would like to put figurines, nay, <em>controversial</em> conversation starters
+			on his desk...Not only for himself, but for the excitement of the whole office.<br />
+
+			<strong>Goals:</strong> He needs to find a figurine that represents his views. He needs
+			to find a figurine just right for that cute receptionist who seems to think he's funny.
+			He needs whimsical gifts for his family and friends, and he wants them all in one place.
+		</p>
+
+		<h1>The Admin</h1>
+		<p>
+			<strong>Name:</strong> Gregory Adobe<br />
+
+			<strong>Age:</strong> 38<br />
+
+			<strong>Profession:</strong> Facilities Coordinator for the New York Giants.<br/>
+
+			<strong>Technology:</strong> Dell's at work, Mac's at home. Has iPhone and iPad with a shared
+			10GB a month plan on Verizon's network.<br />
+
+			<strong>Attitudes and Behaviours:</strong> Greg likes politics in general,
+			but think's most people are silly for believing that anyone is what they
+			say they are. The representatives are just that; a reflection of their constituancy.
+			Of course they lie and flip-flop...people in his state do all the time. He is motivated
+			to profit from the ignorance of people having opinions of these human shells (politicians).<br />
+
+			<strong>Frustrations and Needs:</strong> Greg needs an easy way to add images of mockups
+			and prototype figurines etc to his Chinese-based supplier. He uses Google
+			Trends to analyze the market or popularity of a political figure. He is
+			frustrated that his site that he currently uses is so difficult to navigate
+			and isn't even sure how to check if his customers have their orders in route
+			or not.<br />
+
+			<strong>Goals:</strong> To upload pictures of products, describe them, set
+			and change prices, create sales on products that have backstock, and create
+			and destroy categories and gift sets.
+		</p>
 	</body>
 </html>
