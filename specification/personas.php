d4f449b937b451682c47249b2344e418edf62ead
Kyla Carroll <kylacarroll43@gmail.com>
2015-04-29 16:20:24 -0600

    Added the first persona, the Individual, to personas.php

---
+++ specification/personas.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Capstone Personas</title>
+	</head>
+
+	<body>
+		<h2>The Individual</h2>
+		<p> <strong>Name:</strong> Ted Ernest Bayer<br />
+			<strong>Age:</strong> 32...ish<br />
+			<strong>Profession:</strong> Mr. Bayer is a paper salesman for a small company, often bored by his job and looking for a little clean excitement to add to his often cluttered desk.<br/>
+			<strong>Technology:</strong> Works from a low-end eMachine running Windows Vista, the same model as everyone else in his office. At home he has a budget PC build, accessing our site through Firefox.
+			He doesn't use his Android phone much for accessing the web, due to data caps.<br />
+			<strong> Attitudes and Behaviours:</strong> Ted is an opinionated single guy with a little disposable income who enjoys debating with his colleagues almost as much as he likes to keep up with the Kardashians.
+			Both a political and social media information hound, he spends his time at work arguing with people online and taking BuzzFeed quizzes while attempting to engage his coworkers in friendly banter.<br />
+			<strong> Frustrations and Needs:</strong> Ted is sick of people walking by his desk and not having anything to say to him; he hates having to initiate conversations...
+			So Ted would like to put figurines, nay, <em>controversial</em> conversation starters on his desk...Not only for himself, but for the excitement of the whole office.<br />
+			<strong>Goals:</strong> He needs to find a figurine that represents his views. He needs to find a figurine just right for that cute receptionist who seems to think he's funny.
+			He needs whimsical gifts for his family and friends, and he wants them all in one place.</p>
+	</body>
+</html>
