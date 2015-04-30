9e1cdb20482e3e4f025f8df01405f72df62ce0e2
James Hill <james.w.l.hill@gmail.com>
2015-04-29 20:00:15 -0600

    Added 'bulk buyer' persona to cheqout epic https://app.asana.com/0/32737638783261/32737638783270

--- specification/personas.php
+++ specification/personas.php
@@ -3,7 +3,7 @@
 
 	<head>
 		<meta charset="UTF-8">
-		<title>Capstone Personas</title>
+		<title>Cheqout Personas</title>
 	</head>
 
 	<body>
@@ -66,5 +66,54 @@
 			and change prices, create sales on products that have backstock, and create
 			and destroy categories and gift sets.
 		</p>
+		<h1>The Bulk Buyer</h1>
+		<section>
+			<p>
+				<strong>Name:</strong> Emiliano Zapata<br />
+				<strong>Age:</strong> 52<br />
+			</p>
+			<p>
+				<h3><strong>Profession:</strong></h3>
+				<article>
+					Campaign Strategist for the Democratic National Committee whose delegates have tasked him
+					with harnessing a new popular social symbol which millenials can really sink their teeth into
+					and incorporate into their hip political-satire vlogs, thereby fostering greater party traction
+					among future generations of voters whose primary news source is The Daily Show.
+				</article>
+				<h3><strong>Technology:</strong></h3>
+				<article>
+					Mr. Zapata enjoys the convenience of Apple services and accounts syncing across all of his devices,
+					plus the Committee has provided its key members with official-use only 27-inch 5k retina desktop display iMacs.
+					For personal use, and especially when roaming the country gaining campaign support, Mr. Zapata always carries
+					his new 12-inch retina MacBook Pro, as well as his 4G-LTE unlimited data plan iPhone 6.
+				</article>
+				<h3><strong>Attitudes and Behaviors:</strong></h3>
+				<article>
+					Under constant pressure to deliver meaningful content to the committee, he’s always browsing popular social
+					media sites for novel trends and media to utilize for his political strategies. He’s constantly perusing Facebook,
+					LinkedIn, and Twitter for new material, and rarely spends more than ten seconds looking for inspiration on any given new post.
+				</article>
+				<h3><strong>Frustrations and Needs:</strong></h3>
+				<article>
+					Mr. Zapata needs a constant influx of inspiration and solid, family-friendly, often-humorous material to present
+					to the party leaders. He needs to find novel and catchy symbols to grab a generation’s attention whose attention span
+					is so short, it’s hardly measurable. He’s frustrated by the fleeting nature of his future constituents' political/social
+					concerns, and wants to give them something that they can really relate to and which will solidify a positive association
+					with the Democratic Party.
+				</article>
+				<h3><strong>Goals:</strong></h3>
+				<article>
+					Mr. Zapata has one goal- garner greater support for his party across the younger generations of Americans.
+					To achieve this, he needs to speak to their cultural beliefs and institutions, which are often delivered satirically,
+					especially when politics is the subject. How to reach a generation whose only unshakable belief in the political parties
+					of the day, is that those parties will inevitably act so insensibly that the only serious commentary on their actions
+					must resort to mockery...that is his primary question. This must include popular physical mediums, such as t-shirts
+					and bumper stickers, and anything else that will deliver the message (such as a half-human, half-cyborg, Dick Cheney
+					figurine holding a scale of justice weighted in favor of a money bag emblazoned with the logo of Halliburton against
+					a peace sign). Also, discounted whole-sale orders of said figurine won’t hurt either.
+				</article>
+			</p>
+		</section>
+
 	</body>
 </html>
