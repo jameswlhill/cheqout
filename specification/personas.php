27989b311bf858e551d486123a3d5e97e46f2072
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-01 10:31:09 -0600

    Formatted personas.php and usecases.php https://app.asana.com/0/32737638783261/32737638783270

--- specification/personas.php
+++ specification/personas.php
@@ -1,161 +1,165 @@
 <!DOCTYPE html>
 <html lang="en">
-
 	<head>
-
 		<meta charset="UTF-8">
-
 		<title>Cheqout Personas</title>
-
 	</head>
-
 	<body>
-		<section>
-
-			<h1>The Individual</h1>
-
-			<p>
-				<strong>Name:</strong> Ted Ernest Bayer<br />
-
-				<strong>Age:</strong> 32...ish<br />
-
-				<strong>Profession:</strong> Mr. Bayer is a paper salesman for a small company,
-				often bored by his job and looking for a little clean excitement to add to his
-				often cluttered desk.<br/>
-
-				<strong>Technology:</strong> Works from a low-end eMachine running Windows Vista,
-				the same model as everyone else in his office. At home he has a budget PC build,
-				accessing our site through Firefox. He doesn't use his Android phone much for
-				accessing the web, due to data caps.<br />
-
-				<strong> Attitudes and Behaviours:</strong> Ted is an opinionated single guy with
-				a little disposable income who enjoys debating with his colleagues almost as much
-				as he likes to keep up with the Kardashians. Both a political and social media
-				information hound, he spends his time at work arguing with people online and taking
-				BuzzFeed quizzes while attempting to engage his coworkers in friendly banter.<br />
-
-				<strong> Frustrations and Needs:</strong> Ted is sick of people walking by his desk
-				and not having anything to say to him; he hates having to initiate conversations...
-				So Ted would like to put figurines, nay, <em>controversial</em> conversation starters
-				on his desk...Not only for himself, but for the excitement of the whole office.<br />
-
-				<strong>Goals:</strong> He needs to find a figurine that represents his views. He needs
-				to find a figurine just right for that cute receptionist who seems to think he's funny.
-				He needs whimsical gifts for his family and friends, and he wants them all in one place.
-			</p>
-
-			<h1>The Admin</h1>
-
-			<p>
-				<strong>Name:</strong> Gregory Adobe<br />
-
-				<strong>Age:</strong> 38<br />
-
-				<strong>Profession:</strong> Facilities Coordinator for the New York Giants.<br/>
-
-				<strong>Technology:</strong> Dell's at work, Mac's at home. Has iPhone and iPad with a shared
-				10GB a month plan on Verizon's network.<br />
-
-				<strong>Attitudes and Behaviours:</strong> Greg likes politics in general,
-				but think's most people are silly for believing that anyone is what they
-				say they are. The representatives are just that; a reflection of their constituancy.
-				Of course they lie and flip-flop...people in his state do all the time. He is motivated
-				to profit from the ignorance of people having opinions of these human shells (politicians).<br />
-
-				<strong>Frustrations and Needs:</strong> Greg needs an easy way to add images of mockups
-				and prototype figurines etc to his Chinese-based supplier. He uses Google
-				Trends to analyze the market or popularity of a political figure. He is
-				frustrated that his site that he currently uses is so difficult to navigate
-				and isn't even sure how to check if his customers have their orders in route
-				or not.<br />
-
-				<strong>Goals:</strong> To upload pictures of products, describe them, set
-				and change prices, create sales on products that have back stock, and create
-				and destroy categories and gift sets.
-			</p>
-
-			<h1>The Bulk Buyer</h1>
-			<p>
-				<strong>Name:</strong> Emiliano Zapata<br />
-
-				<strong>Age:</strong> 52<br />
-
-				<strong>Profession:</strong> Campaign Strategist for the Democratic National Committee whose delegates have tasked him
-				with harnessing a new popular social symbol which millenials can really sink their teeth into
-				and incorporate into their hip political-satire vlogs, thereby fostering greater party traction
-				among future generations of voters whose primary news source is The Daily Show.<br />
-
-				<strong>Technology:</strong> Mr. Zapata enjoys the convenience of Apple services and accounts syncing across all of his devices,
-				plus the Committee has provided its key members with official-use only 27-inch 5k retina desktop display iMacs.
-				For personal use, and especially when roaming the country gaining campaign support, Mr. Zapata always carries
-				his new 12-inch retina MacBook Pro, as well as his 4G-LTE unlimited data plan iPhone 6.<br />
-
-				<strong>Attitudes and Behaviors:</strong> Under constant pressure to deliver meaningful content to the committee, he’s always browsing popular social
-				media sites for novel trends and media to utilize for his political strategies. He’s constantly perusing Facebook,
-				LinkedIn, and Twitter for new material, and rarely spends more than ten seconds looking for inspiration on any given new post.<br />
-
-				<strong>Frustrations and Needs:</strong> Mr. Zapata needs a constant influx of inspiration and solid, family-friendly, often-humorous material to present
-				to the party leaders. He needs to find novel and catchy symbols to grab a generation’s attention whose attention span
-				is so short, it’s hardly measurable. He’s frustrated by the fleeting nature of his future constituents' political/social
-				concerns, and wants to give them something that they can really relate to and which will solidify a positive association
-				with the Democratic Party.<br />
-
-				<strong>Goals:</strong> Mr. Zapata has one goal- garner greater support for his party across the younger generations of Americans.
-				To achieve this, he needs to speak to their cultural beliefs and institutions, which are often delivered satirically,
-				especially when politics is the subject. How to reach a generation whose only unshakable belief in the political parties
-				of the day, is that those parties will inevitably act so insensibly that the only serious commentary on their actions
-				must resort to mockery...that is his primary question. This must include popular physical mediums, such as t-shirts
-				and bumper stickers, and anything else that will deliver the message (such as a half-human, half-cyborg, Dick Cheney
-				figurine holding a scale of justice weighted in favor of a money bag emblazoned with the logo of Halliburton against
-				a peace sign). Also, discounted whole-sale orders of said figurine won’t hurt either.
-			</p>
-
-			<h1>The Attacker Threat Model</h1>
-
-			<p>
-				<strong>Name:</strong> Joe King<br />
-
-				<strong>Age:</strong> 48<br />
-
-				<strong>Profession:</strong> Joe is a minimum wage telemarketer by day, and devious opportunist by night. His side income involves
-				SQL injecting weakly-designed databases and dumping confidential information on the dark web.<br />
-
-				<strong>Goals:</strong> Hoe wants to use any and all technology at his disposal to get to the heart of an insecure database
-				that has stored all its customer data in plain text. Adept at creating cross-site-scripts and injecting
-				SQL statements to expose information, he is rarely technologically able to create a denial of service attack
-				but does have access to a large group of other attackers who could and would, given the proper motivation
-				(a secure enough database).<br />
-
-				<strong>Potential Sensitive Information:</strong>
-			</p>
-			<ul>
-				<li>username</li>
-				<li>password</li>
-				<li>email</li>
-				<li>address</li>
-			</ul>
-
-			<p>With the information Joe has obtained, he is able to either sell the information to another malicious user,
-				or attempt to access other secure sites with the login credentials he has obtained. Joe would also be able to
-				orchestrate an act of violence or harassment against any user. <br />
-
-				<strong>Methods of concealing sensitive customer information:</strong></p>
-
-			<ul>
-				<li>Encrypt each customer's Name, user name, password, email, and physical address</li>
-				<li>Utilize Stripe's API to protect credit card information</li>
-			</ul>
-
-			<strong>Table fields which should not be ciphered for system convenience:</strong>
-
-			<ul>
-				<li>Primary key</li>
-				<li>Foreign key references</li>
-				<li>OrderId</li>
-				<li>OrderContents</li>
-				<li>Session id</li>
-				<li>Product details</li>
-			</ul>
-		</section>
+		<h1>The Individual</h1>
+		<p>
+		<strong>Name:</strong> Ted Ernest Bayer
+		</p>
+		<p>
+		<strong>Age:</strong> 32...ish
+		</p>
+		<p>
+		<strong>Profession:</strong> Mr. Bayer is a paper salesman for a small company,
+		often bored by his job and looking for a little clean excitement to add to his
+		often cluttered desk.
+		</p>
+		<p>
+		<strong>Technology:</strong> Works from a low-end eMachine running Windows Vista,
+		the same model as everyone else in his office. At home he has a budget PC build,
+		accessing our site through Firefox. He doesn't use his Android phone much for
+		accessing the web, due to data caps.
+		</p>
+		<p>
+		<strong> Attitudes and Behaviours:</strong> Ted is an opinionated single guy with
+		a little disposable income who enjoys debating with his colleagues almost as much
+		as he likes to keep up with the Kardashians. Both a political and social media
+		information hound, he spends his time at work arguing with people online and taking
+		BuzzFeed quizzes while attempting to engage his coworkers in friendly banter.
+		</p>
+		<p>
+		<strong> Frustrations and Needs:</strong> Ted is sick of people walking by his desk
+		and not having anything to say to him; he hates having to initiate conversations...
+		So Ted would like to put figurines, nay, <em>controversial</em> conversation starters
+		on his desk...Not only for himself, but for the excitement of the whole office.
+		</p>
+		<p>
+		<strong>Goals:</strong> He needs to find a figurine that represents his views. He needs
+		to find a figurine just right for that cute receptionist who seems to think he's funny.
+		He needs whimsical gifts for his family and friends, and he wants them all in one place.
+		</p>
+		<hr />
+		<h1>The Admin</h1>
+		<p>
+		<strong>Name:</strong> Gregory Adobe
+		</p>
+		<p>
+		<strong>Age:</strong> 38
+		<p>
+		<strong>Profession:</strong> Facilities Coordinator for the New York Giants.
+		</p>
+		<p>
+		<strong>Technology:</strong> Dell's at work, Mac's at home. Has iPhone and iPad with a shared
+		10GB a month plan on Verizon's network.
+		</p>
+		<p>
+		<strong>Attitudes and Behaviours:</strong> Greg likes politics in general,
+		but thinks most people are silly for believing that anyone is what they
+		say they are. The representatives are just that; a reflection of their constituancy.
+		Of course they lie and flip-flop...people in his state do all the time. He is motivated
+		to profit from the ignorance of people having opinions of these human shells (politicians).
+		</p>
+		<p>
+		<strong>Frustrations and Needs:</strong> Greg needs an easy way to add images of mockups
+		and prototype figurines etc to his Chinese-based supplier. He uses Google
+		Trends to analyze the market or popularity of a political figure. He is
+		frustrated that his site that he currently uses is so difficult to navigate
+		and isn't even sure how to check if his customers have their orders in route
+		or not.
+		</p>
+		<p>
+		<strong>Goals:</strong> To upload pictures of products, describe them, set
+		and change prices, create sales on products that have back stock, and create
+		and destroy categories and gift sets.
+		</p>
+		<hr />
+		<h1>The Bulk Buyer</h1>
+		<p>
+		<strong>Name:</strong> Emiliano Zapata<br />
+		<strong>Age:</strong> 52<br />
+		</p>
+		<p>
+		<strong>Profession:</strong> Campaign Strategist for the Democratic National Committee whose delegates
+		have tasked him with harnessing a new popular social symbol which millenials can really sink their teeth into
+		and incorporate into their hip political-satire vlogs, thereby fostering greater party traction
+		among future generations of voters whose primary news source is The Daily Show.
+		</p>
+		<p>
+		<strong>Technology:</strong> Mr. Zapata enjoys the convenience of Apple services and accounts syncing
+		across all of his devices, plus the Committee has provided its key members with official-use only 27-inch 5k
+		retina desktop display iMacs. For personal use, and especially when roaming the country gaining campaign
+		support, Mr. Zapata always carries his new 12-inch retina MacBook Pro, as well as his 4G-LTE unlimited data
+		plan iPhone 6.
+		</p>
+		<p>
+		<strong>Attitudes and Behaviors:</strong> Under constant pressure to deliver meaningful content to the
+		committee, he’s always browsing popular social media sites for novel trends and media to utilize for his
+		political strategies. He’s constantly perusing Facebook, LinkedIn, and Twitter for new material, and rarely
+		spends more than ten seconds looking for inspiration on any given new post.
+		</p>
+		<p>
+		<strong>Frustrations and Needs:</strong> Mr. Zapata needs a constant influx of inspiration and solid,
+		family-friendly, often-humorous material to present to the party leaders. He needs to find novel and catchy
+		symbols to grab a generation’s attention whose attention span is so short, it’s hardly measurable. He’s
+		frustrated by the fleeting nature of his future constituents' political/social concerns, and wants to give
+		them something that they can really relate to and which will solidify a positive association with the
+		Democratic Party.
+		</p>
+		<p>
+		<strong>Goals:</strong> Mr. Zapata has one goal- garner greater support for his party across the younger generations of Americans.
+		To achieve this, he needs to speak to their cultural beliefs and institutions, which are often delivered satirically,
+		especially when politics is the subject. How to reach a generation whose only unshakable belief in the political parties
+		of the day, is that those parties will inevitably act so insensibly that the only serious commentary on their actions
+		must resort to mockery...that is his primary question. This must include popular physical mediums, such as t-shirts
+		and bumper stickers, and anything else that will deliver the message (such as a half-human, half-cyborg, Dick Cheney
+		figurine holding a scale of justice weighted in favor of a money bag emblazoned with the logo of Halliburton against
+		a peace sign). Also, discounted whole-sale orders of said figurine won’t hurt either.
+		</p>
+		<hr />
+		<h1>The Attacker Threat Model</h1>
+		<p>
+		<strong>Name:</strong> Joe King
+		</p>
+		<p>
+		<strong>Age:</strong> 48
+		</p>
+		<p>
+		<strong>Profession:</strong> Joe is a minimum wage telemarketer by day, and devious opportunist by night. His side income involves
+		SQL injecting weakly-designed databases and dumping confidential information on the dark web.<br />
+		<strong>Goals:</strong> Hoe wants to use any and all technology at his disposal to get to the heart of an insecure database
+		that has stored all its customer data in plain text. Adept at creating cross-site-scripts and injecting
+		SQL statements to expose information, he is rarely technologically able to create a denial of service attack
+		but does have access to a large group of other attackers who could and would, given the proper motivation
+		(a secure enough database).
+		</p>
+		<strong>Potential Sensitive Information:</strong>
+		<ul>
+			<li>username</li>
+			<li>password</li>
+			<li>email</li>
+			<li>address</li>
+		</ul>
+		<p>
+		With the information Joe has obtained, he is able to either sell the information to another malicious user,
+		or attempt to access other secure sites with the login credentials he has obtained. Joe would also be able to
+		orchestrate an act of violence or harassment against any user.
+		</p>
+		<strong>Methods of concealing sensitive customer information:</strong></p>
+		<ul>
+			<li>Encrypt each customer's Name, user name, password, email, and physical address</li>
+			<li>Utilize Stripe's API to protect credit card information</li>
+		</ul>
+		<strong>Table fields which should not be ciphered for system convenience:</strong>
+		<ul>
+			<li>Primary key</li>
+			<li>Foreign key references</li>
+			<li>OrderId</li>
+			<li>OrderContents</li>
+			<li>Session id</li>
+			<li>Product details</li>
+		</ul>
 	</body>
 </html>
