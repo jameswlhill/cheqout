5b9494459d891b19fb02c45b1fc6ad9881f29908
Kyla Carroll <kylacarroll43@gmail.com>
2015-05-01 09:10:30 -0600

    Updated and reformatted the threat model on personas.php, and started usecase of The Individual.
    https://app.asana.com/0/32737638783261/32737638783270

--- specification/personas.php
+++ specification/personas.php
@@ -67,43 +67,28 @@
 			and destroy categories and gift sets.
 		</p>
 		<h1>The Bulk Buyer</h1>
-		<section>
 			<p>
 				<strong>Name:</strong> Emiliano Zapata<br />
+
 				<strong>Age:</strong> 52<br />
-			</p>
-			<p>
-				<h3><strong>Profession:</strong></h3>
-				<article>
-					Campaign Strategist for the Democratic National Committee whose delegates have tasked him
+
+				<strong>Profession:</strong> Campaign Strategist for the Democratic National Committee whose delegates have tasked him
 					with harnessing a new popular social symbol which millenials can really sink their teeth into
 					and incorporate into their hip political-satire vlogs, thereby fostering greater party traction
-					among future generations of voters whose primary news source is The Daily Show.
-				</article>
-				<h3><strong>Technology:</strong></h3>
-				<article>
-					Mr. Zapata enjoys the convenience of Apple services and accounts syncing across all of his devices,
+					among future generations of voters whose primary news source is The Daily Show.<br />
+				<strong>Technology:</strong> Mr. Zapata enjoys the convenience of Apple services and accounts syncing across all of his devices,
 					plus the Committee has provided its key members with official-use only 27-inch 5k retina desktop display iMacs.
 					For personal use, and especially when roaming the country gaining campaign support, Mr. Zapata always carries
-					his new 12-inch retina MacBook Pro, as well as his 4G-LTE unlimited data plan iPhone 6.
-				</article>
-				<h3><strong>Attitudes and Behaviors:</strong></h3>
-				<article>
-					Under constant pressure to deliver meaningful content to the committee, he’s always browsing popular social
+					his new 12-inch retina MacBook Pro, as well as his 4G-LTE unlimited data plan iPhone 6.<br />
+				<strong>Attitudes and Behaviors:</strong> Under constant pressure to deliver meaningful content to the committee, he’s always browsing popular social
 					media sites for novel trends and media to utilize for his political strategies. He’s constantly perusing Facebook,
-					LinkedIn, and Twitter for new material, and rarely spends more than ten seconds looking for inspiration on any given new post.
-				</article>
-				<h3><strong>Frustrations and Needs:</strong></h3>
-				<article>
-					Mr. Zapata needs a constant influx of inspiration and solid, family-friendly, often-humorous material to present
+					LinkedIn, and Twitter for new material, and rarely spends more than ten seconds looking for inspiration on any given new post.<br />
+				<strong>Frustrations and Needs:</strong> Mr. Zapata needs a constant influx of inspiration and solid, family-friendly, often-humorous material to present
 					to the party leaders. He needs to find novel and catchy symbols to grab a generation’s attention whose attention span
 					is so short, it’s hardly measurable. He’s frustrated by the fleeting nature of his future constituents' political/social
 					concerns, and wants to give them something that they can really relate to and which will solidify a positive association
-					with the Democratic Party.
-				</article>
-				<h3><strong>Goals:</strong></h3>
-				<article>
-					Mr. Zapata has one goal- garner greater support for his party across the younger generations of Americans.
+					with the Democratic Party.<br />
+				<strong>Goals:</strong> Mr. Zapata has one goal- garner greater support for his party across the younger generations of Americans.
 					To achieve this, he needs to speak to their cultural beliefs and institutions, which are often delivered satirically,
 					especially when politics is the subject. How to reach a generation whose only unshakable belief in the political parties
 					of the day, is that those parties will inevitably act so insensibly that the only serious commentary on their actions
@@ -111,33 +96,24 @@
 					and bumper stickers, and anything else that will deliver the message (such as a half-human, half-cyborg, Dick Cheney
 					figurine holding a scale of justice weighted in favor of a money bag emblazoned with the logo of Halliburton against
 					a peace sign). Also, discounted whole-sale orders of said figurine won’t hurt either.
-				</article>
 			</p>
-		</section>
 		<h1>The Attacker Threat Model</h1>
 		<section>
 			<p>
-				<strong>Name: </strong>Kevin Mitnick
-				<strong>Age: </strong>48
-			</p>
-			<p>
-				<strong>Profession:</strong>
-				Kevin is a minimum wage telemarketer by day, and devious opportunist by night. His side income involves
-				SQL injecting weakly-designed databases and dumping confidential information on the dark web.
-			</p>
-			<p>
-				<strong>Goals:</strong>
-				Kevin wants to use any and all technology at his disposal to get to the heart of an insecure database
-				that has stored all its customer data in plain text.
-			</p>
-			<p>
-				<strong>Methods of concealing sensitive customer information from the likes of Kevin:</strong>
+				<strong>Name:</strong> Kevin Mitnick<br />
+				<strong>Age:</strong> 48<br />
+				<strong>Profession:</strong> Kevin is a minimum wage telemarketer by day, and devious opportunist by night. His side income involves
+				SQL injecting weakly-designed databases and dumping confidential information on the dark web.<br />
+				<strong>Goals:</strong> Kevin wants to use any and all technology at his disposal to get to the heart of an insecure database
+				that has stored all its customer data in plain text. Adept at creating cross-site-scripts and injecting
+				SQL statements to expose information, he is rarely technologically able to create a denial of service attack
+				but does have access to a large group of other attackers who could and would, given the proper motivation
+				(a secure enough database).<br />
+				<strong>Methods of concealing sensitive customer information:</strong></p>
 				<ul>
-					<li>Encrypt each customer's Name, user name, password, and physical address</li>
+					<li>Encrypt each customer's Name, user name, password, email, and physical address</li>
 					<li>Utilize Stripe's API to protect credit card information</li>
 				</ul>
-			</p>
-			<p>
 				<strong>Table fields which should not be ciphered for system convenience:</strong>
 				<ul>
 					<li>Primary key</li>
@@ -147,7 +123,5 @@
 					<li>Session id</li>
 					<li>Product details</li>
 				</ul>
-			</p>
-		</section>
 	</body>
 </html>
---
+++ specification/usecases.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Capstone Use Cases</title>
+	</head>
+	<body>
+		<h1>The Individual</h1>
+		<p>It's just another boring day for Mr. Bayer at DM Paper, and instead of boosting his sales, Ted decides he is going to browse the internet (strangely, not a foreign concept for him).
+		Ted is completing a "Which Quirky Sitcom Character Are You?" quiz when he notices an ad on the last of the 12 pages he's had to click through to get to his answer. It is an ad featuring
+		several of our political and humorous figurines, along with some cartooney words promising that they're "perfect for gifts or your desk!". Intrigued, he opens the site in a new tab.<br />
+		Finished clicking through the 'funny' galleries linked to him on Facebook (#7 will blow your mind!), the last tab that is open is our home page; Ted vaguely recalls opening it, and </p>
+	</body>
+</html>
