5b238fb1497fdd7eab349809d6f027c2b7c59d25
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-01 10:44:18 -0600

    Formatted usecases.php to match personas.php
    https://app.asana.com/0/32737638783261/32737638783270

--- specification/usecases.php
+++ specification/usecases.php
@@ -8,39 +8,39 @@
 	<body>
 		<h1>The Individual</h1>
 		<p>
-		It's just another boring day for Mr. Bayer at DM Paper, and instead of boosting his
-		sales, Ted decides he is going to browse the internet (strangely, not a foreign concept for him).
-		Ted is completing a "Which Quirky Sitcom Character Are You?" quiz when he notices an ad on the
-		last of the 12 pages he's had to click through to get to his answer. It is an ad featuring
-		several of our political and humorous figurines, along with some cartooney words promising
-		that they're "perfect for gifts or your desk!". Intrigued, he opens the site in a new tab.
+			It's just another boring day for Mr. Bayer at DM Paper, and instead of boosting his
+			sales, Ted decides he is going to browse the internet (strangely, not a foreign concept for him).
+			Ted is completing a "Which Quirky Sitcom Character Are You?" quiz when he notices an ad on the
+			last of the 12 pages he's had to click through to get to his answer. It is an ad featuring
+			several of our political and humorous figurines, along with some cartooney words promising
+			that they're "perfect for gifts or your desk!". Intrigued, he opens the site in a new tab.
 		</p>
 		<p>
-		Finished clicking through the 'funny' galleries linked to him on Facebook (#7 will blow your
-		mind!), the last tab that is open is our home page; Ted vaguely recalls opening it, and
+			Finished clicking through the 'funny' galleries linked to him on Facebook (#7 will blow your
+			mind!), the last tab that is open is our home page; Ted vaguely recalls opening it, and
 		</p>
+		<hr />
 		<h1>The Admin</h1>
 		<p>
-		Using his iPad on a lunch break to add another figurine to his online
-		marketplace, Gregory attempts to add the governor local to his state,
-		being a possible Presidential candidate in the near future. He signs in to
-		the administrator account he has permissions on, and navigates to the
-		product page. He clicks a gigantic "Add..." button on his
-		administrator bar. Greg uses a popup to add a picture of the prototype of
-		his governor that he has locally stored on his iPad. The upload takes a second,
-		then he is taken to Add Product page, where he can select the category,
-		whether the item is New (and therefore should be on the front page), and place
-		tags for search-ability in (state name, governor tag, political tag etc.).
+			Using his iPad on a lunch break to add another figurine to his online
+			marketplace, Gregory attempts to add the governor local to his state,
+			being a possible Presidential candidate in the near future. He signs in to
+			the administrator account he has permissions on, and navigates to the
+			product page. He clicks a gigantic "Add..." button on his
+			administrator bar. Greg uses a popup to add a picture of the prototype of
+			his governor that he has locally stored on his iPad. The upload takes a second,
+			then he is taken to Add Product page, where he can select the category,
+			whether the item is New (and therefore should be on the front page), and place
+			tags for search-ability in (state name, governor tag, political tag etc.).
 		</p>
 		<p>
-		He doesn't have enough time to finish just yet, so he puts away his iPad after
-		his lunch break is over, and is able to resume the process later at home.
+			He doesn't have enough time to finish just yet, so he puts away his iPad after
+			his lunch break is over, and is able to resume the process later at home.
 		</p>
 		<p>
-		After adding all required fields, he taps Submit on the Add... page. Now if
-		a customer checks the site, the update will display as a new product, and display
-		prominantly on the front page, and in it's respective place in the categories
-		product pages.
+			Later, on his Macbook, after adding all required fields, he clicks Submit. Now if
+			a customer checks the site, the update will display as a new product, and display
+			prominently on the front page in addition to it's respective place on product pages.
 		</p>
 	</body>
 </html>
