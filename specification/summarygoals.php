bd83fc63dc80130410cf98e89bc0562818995984
Kyla Carroll <kylacarroll43@gmail.com>
2015-05-01 10:39:15 -0600

    edited summarygoals.php, removed purpose.php as summary and goals already had a page dedicated
    https://app.asana.com/0/32737638783261/32737638783270

---
+++ specification/summarygoals.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>System Summary & Goals</title>
+	</head>
+	<body>
+		<section>
+			<h1>System Summary</h1>
+			<p> The purpose of this eCommerce website, CQ, is to provide humorous, topical figurines based on the perceived
+				good side/bad side ofpolitical figures,celebrities, and public figures for use by individual consumers and by
+				organizations The CQ site will be the “face” and main communications/sales channel for CQ for both consumer and
+				business customers. Products for individuals can be used for gifts, decor, art projects, kid’s parties, etc.
+				Organizational clients such as corporations, election campaigns, sports events and similar bulk users will use
+				CQ figurine products as giveaways in promotions, trade shows, campaign events, employee programs and more. Our
+				competitive advantage include’s CQ’s ability to produce quality, unique products with rapid turnaround and mass
+				production.</p>
+			<h2>Audiences</h2>
+			<p>The site will serve the following audiences:</p>
+			<ul>
+				<li>Consumer Users of all/any type</li>
+				<li>Organizational Customers that buy in bulk quantities</li>
+			</ul>
+			<br />
+			<h2>Goals</h2>
+			<p>The primary goal is for the site to be CQ’s main sales channel by providing highly marketable products.
+				As such, CQ will be positioned as a product innovator as well as a commercial success, enabling the site to:</p>
+			<ul>
+				<li>Draw repeat and potential clients to the company</li>
+				<li>Provide a showcase for our work</li>
+				<li>Build CQ’s brand via consistent, quality customer experiences</li>
+				<li>Expand the depth and breadth of our Internet presence</li>
+			</ul>
+		</section>
+	</body>
+</html>
