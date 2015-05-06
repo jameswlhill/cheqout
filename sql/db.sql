d43ebd8e3b70ec56434a28c6d77e826d1a91dd14
James Hill <james.w.l.hill@gmail.com>
2015-05-06 16:59:57 -0600

    Made suggested changes to DDL https://app.asana.com/0/31506975298102/33387218874594

--- sql/db.sql
+++ sql/db.sql
@@ -9,35 +9,38 @@ DROP TABLE IF EXISTS email;
 
 CREATE TABLE email (
 	emailId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	emailAddress UNIQUE VARCHAR (128) NOT NULL,
+	emailAddress VARCHAR (128) NOT NULL,
 	stripeId VARCHAR (25) NOT NULL,
-	PRIMARY KEY (emailId)
+	PRIMARY KEY(emailId),
+	UNIQUE(emailAddress)
 );
 
 CREATE TABLE account (
 	accountId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	accountPassword CHAR (128) NOT NULL,
 	accountPasswordSalt CHAR (64) NOT NULL,
-	activation UNIQUE CHAR (32) NULLABLE,
+	activation CHAR (32),
 	accountCreateDateTime DATETIME NOT NULL,
 	emailId INT UNSIGNED NOT NULL,
 	INDEX(emailId),
 	FOREIGN KEY(emailId) REFERENCES email(emailId),
-	PRIMARY KEY(accountId)
+	PRIMARY KEY(accountId),
+	UNIQUE(activation)
+
 );
 
 CREATE TABLE guest (
-	guestId INT AUTO_INCREMENT NOT NULL,
-	emailId INT NOT NULL,
+	guestId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	emailId INT UNSIGNED NOT NULL,
 	INDEX(emailId),
 	FOREIGN KEY(emailId) REFERENCES email(emailId),
 	PRIMARY KEY(guestId)
 );
 
 CREATE TABLE address (
-	addressId INT AUTO_INCREMENT NOT NULL,
+	addressId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	emailId VARCHAR (128) NOT NULL,
-	addressHidden CHAR(1) NOT NULL,
+	addressHidden TINYINT(1) UNSIGNED NOT NULL,
 	addressAttention VARCHAR (100) NOT NULL,
 	addressStreet1 VARCHAR (128) NOT NULL,
 	addressStreet2 VARCHAR (128),
@@ -54,17 +57,17 @@ CREATE TABLE address (
 CREATE TABLE product (
 	productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	productTitle VARCHAR (128) NOT NULL,
-	productPrice FLOAT(20,2) NOT NULL,
+	productPrice DECIMAL(20,2) NOT NULL,
 	productDescription VARCHAR (500) NOT NULL,
-	productInventory INT NULLABLE,
-	productSale INT NULLABLE,
+	productInventory INT UNSIGNED,
+	productSale INT UNSIGNED,
 	PRIMARY KEY(productId)
 );
 
 CREATE TABLE cheqoutOrder (
-	orderId INT AUTO_INCREMENT NOT NULL,
+	orderId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	emailId VARCHAR (128) NOT NULL,
-	addressId INT NOT NULL,
+	addressId INT UNSIGNED NOT NULL,
 	stripeId VARCHAR (25) NOT NULL,
 	orderDateTime DATETIME NOT NULL,
 	INDEX(emailId),
@@ -75,15 +78,16 @@ CREATE TABLE cheqoutOrder (
 );
 
 CREATE TABLE productOrder (
-	orderId INT NOT NULL,
-	productId INT NOT NULL,
-	quantity INT NOT NULL,
-	shippingCost FLOAT (10,2) NOT NULL,
-	orderPrice FLOAT (20,2) NOT NULL,
+	orderId INT UNSIGNED NOT NULL,
+	productId INT UNSIGNED NOT NULL,
+	quantity INT UNSIGNED NOT NULL,
+	shippingCost DECIMAL(10,2) NOT NULL,
+	orderPrice DECIMAL(20,2) NOT NULL,
 	INDEX(orderId),
 	INDEX(orderId),
 	FOREIGN KEY(orderId) REFERENCES cheqoutOrder(orderId),
-	FOREIGN KEY(productId) REFERENCES product(productId)
+	FOREIGN KEY(productId) REFERENCES product(productId),
+	PRIMARY KEY(orderId,productId)
 );
 
 
