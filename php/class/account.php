aa53f51ef580b0c7f21a3d705c8aff8d9ad7d672
Kyla Carroll <kylacarroll43@gmail.com>
2015-05-07 16:53:25 -0600

    created account class
    https://app.asana.com/0/31506975298069/33387218874589

--- php/class/account.php
+++ php/class/account.php
@@ -2,6 +2,11 @@
 /**
  * Account class for Cheqout
  *
+ * The account class contains both the password (hashed) and
+ * the salt used to further protect the hashed password.
+ * Also present is the activation code used to activate the email
+ * attached to the account, if any.
+ *
  * @author Kyla Carroll <kylacarroll43@gmail.com
  **/
 
@@ -9,10 +14,14 @@
 class Account {
 	//Primary Key for the account
 	private $accountId;
-	//unique email attached to the account
+	//unique emailId attached to the account
 	private $emailId;
-	//account name specified
-	private $email;
+	//hashed password
+	private $accountPassword;
+	//salt for password
+	private $accountPasswordSalt;
+	//unique activation string
+	private $accountActivation;
 	//account creation date
 	private $accountCreateDateTime;
 
@@ -33,12 +42,12 @@ class Account {
 	 * @throws UnexpectedValueException if $newProfileId is not an integer
 	 **/
 
-	public function setAccountId ($newAccountId) {
-		if ($newAccountId === null) {
+	public function setAccountId($newAccountId) {
+		if($newAccountId === null) {
 			return;
 		}
 		$newAccountId = filter_var($newAccountId, FILTER_VALIDATE_INT);
-		if ($newAccountId === false) {
+		if($newAccountId === false) {
 			throw(new UnexpectedValueException("accountId is an invalid integer"));
 		}
 		//assign and store the filtered accountId
@@ -46,94 +55,148 @@ class Account {
 	}
 
 	/**
-	 * accessor method for email
+	 * accessor method for emailId
 	 *
-	 * @return string value of email
+	 * @return int value of emailId
 	 **/
-	public function getEmail() {
-		return ($this->email);
+	public function getEmailId() {
+		return ($this->emailId);
 	}
 
 	/**
-	 * mutator method for email
+	 * mutator method for emailId
 	 *
-	 * @param string $newEmail new value of email
-	 * @throws UnexpectedValueException if $newEmail is not a valid email
+	 * @param string $newEmailId new value of emailId
+	 * @throws UnexpectedValueException if $newEmailId is not a valid integer
 	 **/
-	public function setEmail($newEmail) {
-		$newEmail = filter_var($newEmail, FILTER_SANITIZE_EMAIL);
-		if($newEmail === false) {
-			throw(new UnexpectedValueException("email is not valid"));
+	public function setEmailId($newEmailId) {
+		$newEmailId = filter_var($newEmailId, FILTER_VALIDATE_INT);
+		if($newEmailId === false) {
+			throw(new UnexpectedValueException("emailId is not valid integer"));
 		}
 		//assign and store the email
-		$this->email = $newEmail;
-	}
-	/**
-	 * accessor method for account name
-	 *
-	 * @return string value of account name
-	 */
-	public function getAccountName() {
-		return ($this->accountName);
+		$this->emailId = $newEmailId;
 	}
 
-	/** mutator method for account name
+	/**
+	 * accessor method for account Password
 	 *
-	 * @param string $newAccountName new value of account name
-	 * @throws UnexpectedValueException if $newAccountName is not valid
+	 * @return string value of account password
+	 */
+	public function getAccountPassword() {
+		return ($this->accountPassword);
+	}
+
+	/** mutator method for account password
+	 *
+	 * @param string $newAccountPassword new value of account password
+	 * @throws UnexpectedValueException if $newAccountPassword is not valid
 	 **/
-	public function setAccountName($newAccountName) {
-		$newAccountName = filter_var($newAccountName, FILTER_SANITIZE_STRING);
-		if ($newAccountName === false) {
-			throw(new UnexpectedValueException("account name is not valid"));
+	public function setAccountPassword($newAccountPassword) {
+		$newAccountPassword = filter_var($newAccountPassword, FILTER_SANITIZE_STRING);
+		if($newAccountPassword === false) {
+			throw(new UnexpectedValueException("password is not valid"));
 		}
 		//assign and store Account name
-		$this->accountName = $newAccountName;
+		$this->accountPassword = $newAccountPassword;
 	}
 
 	/**
-	 * accountDate accessor method
+	 * accessor method for account Password salt
+	 *
+	 * @return string value of account pw salt
+	 */
+	public function getAccountPasswordSalt() {
+		return ($this->accountPasswordSalt);
+	}
+
+	/** mutator method for account password salt
+	 *
+	 * @param string $newAccountPasswordSalt new value of account password salt
+	 * @throws UnexpectedValueException if $newAccountPasswordSalt is not valid
+	 **/
+	public function setAccountPasswordSalt($newAccountPasswordSalt) {
+		$newAccountPasswordSalt = filter_var($newAccountPasswordSalt, FILTER_SANITIZE_STRING);
+		if($newAccountPasswordSalt === false) {
+			throw(new UnexpectedValueException("salt invalid"));
+		}
+		//assign and store Account name
+		$this->accountPasswordSalt = $newAccountPasswordSalt;
+	}
+
+	/**
+	 * accountCreateDateTime accessor method
 	 *
 	 * @return string date of account creation
 	 */
-	public function getAccountDate() {
-		return ($this->accountDate);
+	public function getAccountCreateDateTime() {
+		return ($this->accountCreateDateTime);
 	}
 
 	/**
 	 * mutator method for account creation date
 	 *
-	 * @param string $newAccountDate new value of account creation date
-	 * @throws UnexpectedValueException if $newAccountDate is not valid
+	 * @param string $newAccountCreateDateTime new value of account creation date
+	 * @throws UnexpectedValueException if $newAccountCreateDateTime is not valid
 	 */
-	public function setAccountDate($newAccountDate) {
-		$newAccountDate = filter_var($newAccountDate, FILTER_SANITIZE_STRING);
-		if ($newAccountDate === false) {
+	public function setAccountCreateDateTime($newAccountCreateDateTime) {
+		$newAccountCreateDateTime = filter_var($newAccountCreateDateTime, FILTER_SANITIZE_STRING);
+		if($newAccountCreateDateTime === false) {
 			throw(new UnexpectedValueException("account creation date invalid"));
 		}
 		//assign and store account date
-		$this->accountDate = $newAccountDate;
+		$this->accountCreateDateTime = $newAccountCreateDateTime;
+	}
+
+	/**
+	 * accessor method for account activation code
+	 *
+	 * @return string value of account activation code
+	 */
+	public function getAccountActivation() {
+		return ($this->accountActivation);
+	}
+
+	/** mutator method for account activation
+	 *
+	 * @param string $newAccountActivation new value of account activation code
+	 * @throws UnexpectedValueException if $newAccountActivation is not valid
+	 **/
+	public function setAccountActivation($newAccountActivation) {
+		$newAccountActivation = filter_var($newAccountActivation, FILTER_SANITIZE_STRING);
+		if($newAccountActivation === false) {
+			throw(new UnexpectedValueException("account activation invalid"));
+		}
+		//assign and store Account name
+		$this->accountActivation = $newAccountActivation;
 	}
 
 	/**
 	 * constructor magic method for the account
 	 *
 	 * @param int $newAccountId new value for account id
-	 * @param string $newEmail new value for email
-	 * @param string $newAccountName new value for account name
+	 * @param int $newEmailId new value for emailId
+	 * @param string $newAccountPassword new value for password
+	 * @param string $newAccountPasswordSalt new value for account pw salt
+	 * @param string $newAccountCreateDateTime new value for account create date
+	 * @param string $newAccountActivation new value for activation key
+	 *
 	 * @throws UnexpectedValueException if any of the parameters are not valid
 	 **/
-	public function __construct($newAccountId, $newEmail, $newAccountName, $newAccountDate) {
+	public function __construct($newAccountId, $newEmailId, $newAccountPassword, $newAccountPasswordSalt, $newAccountCreateDateTime, $newAccountActivation) {
 		try {
 			$this->setAccountId($newAccountId);
-			$this->setEmail($newEmail);
-			$this->setAccountName($newAccountName);
-			$this->setAccountDate($newAccountDate);
+			$this->setEmailId($newEmailId);
+			$this->setAccountPassword($newAccountPassword);
+			$this->setAccountPasswordSalt($newAccountPasswordSalt);
+			$this->setAccountCreateDateTime($newAccountCreateDateTime);
+			$this->setAccountActivation($newAccountActivation);
 		} catch(UnexpectedValueException $exception) {
 			//rethrow to caller
 			throw(new UnexpectedValueException("unable to construct account", 0, $exception));
 		}
 	}
+
 	/**
 	 * inserts this account into mySQL
 	 *
@@ -147,16 +210,18 @@ class Account {
 		}
 
 		//create the pdo query template
-		$query = "INSERT INTO account(email, accountName, accountDate) VALUES(:email, :accountName, :accountDate)";
+		$query = "INSERT INTO account(emailId, accountPassword, accountPasswordSalt, accountCreateDateTime, activation) VALUES(:emailId, :accountPassword, :accountPasswordSalt, :accountCreateDateTime, :activation)";
 		$statement = $pdo->prepare($query);
 
 		//match the variables input with the query
-		$parameters = array("email" => $this->email, "accountName" => $this->accountName, "accountDate" => $this->accountDate);
+		$parameters = array("emailId" => $this->emailId, "accountPassword" => $this->accountPassword, "accountCreateDateTime" => $this->accountCreateDateTime, "accountActivation" => $this->accountActivation);
 		$statement->execute($parameters);
 
 		//updates the null accountId with the value of the variable
 		$this->accountId = intval($pdo->lastInsertId());
+		$this->emailId = intval($pdo->lastInsertId());
 	}
+
 	/**
 	 * deletes the account from mySQL
 	 *
@@ -190,10 +255,11 @@ class Account {
 			throw(new PDOException("unable to update account that does not exist"));
 		}
 		//query template
-		$query = "UPDATE account SET email = :email, accountName = :accountName, accountDate = :accountDate WHERE accountId = :accountId";
+		$query = "UPDATE account SET emailId = :emailId, accountPassword = :accountPassword, accountPasswordSalt = :accountPasswordSalt, accountCreateDateTime = :accountCreateDateTime, activation = :accountActivation WHERE accountId = :accountId";
 		$statement = $pdo->prepare($query);
 
 		//match the variables to the placeholders in query
-		$parameters = array("email" => $this->email, "accountName" => $this->accountName, "accountDate" => $this->accountDate, "accountId" => $this->accountId);
+		$parameters = array("emailId" => $this->emailId, "accountPassword" => $this->accountPassword, "accountPasswordSalt" => $this->accountPasswordSalt, "accountCreateDateTime" => $this->accountCreateDateTime, "activation" => $this->accountActivation, "accountId" => $this->accountId);
 		$statement->execute($parameters);
 	}
+}
