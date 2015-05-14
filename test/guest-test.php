c1ced1734709f096b66b98c67856f0178b95243f
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-14 10:33:41 -0600

    Gonna try these coverage tests ONE at a time, rather than make 10 and troubleshoot them all at once...
    https://app.asana.com/0/31506975298080/33387218874580

--- test/guest-test.php
+++ test/guest-test.php
@@ -1,55 +1,54 @@
 <?php
+// grab the project test parameters
+require_once("cheqouttest.php");
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/php/class/guest.php");
+require_once(dirname(__DIR__) . "/php/class/email.php");
+
+
 /**
- * Test for address.php
+ * Full PHPUnit test for the Guest class
  *
- * Loads address.php, connects to MySQL database,
- * instantiates a new Address object via the class' constructor magic method,
- * inserts that Address object into database,
- * updates the Address object in the database,
- * selects/reads the Address database entry,
- * update the Address to be hidden
- * and finally, deletes that Product entry from the database
+ * This is a complete PHPUnit test of the Guest class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
  *
- * @author Tyler Wiegand <tyler dot wiegand at me dot com>
+ * @see Guest
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
  **/
-//first, require the system library AES 256 functions
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+class GuestTest extends CheqoutTest {
+	/**
+	 * EmailId that created the Guest; this is for foreign key relations
+	 * @var Address address
+	 **/
+	protected $VALID_EMAIL = null;
 
-//load product.php, class to be tested
-require_once(dirname(__DIR__) . '/php/class/guest.php');
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp() {
+		// though we are making ANOTHER setup, run the first setup first
+		parent::setUp();
+		// create and insert a email to own the test address
+		$this->VALID_EMAIL = new Email(null, "phpunittest@phpunittester.com", "stripeID");
+		$this->VALID_EMAIL->insert($this->getPDO());
+		return;
+	}
+	/**
+	 * test inserting a valid guest and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidGuest() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("guest");
 
-try {
-	$config = readConfig("/etc/apache2/capstone-mysql/cheqout.ini");
-	$dsn = "mysql:host=" . $config["hostname"] . ";dbname=" . $config["database"];
-	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-	$pdoConnection = new PDO($dsn, $config["username"], $config["password"], $options);
-	$pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// create a new Address and insert to into mySQL
+		$guest = new Guest(null, $this->VALID_EMAIL->getEmailId());
+		$guest->insert($this->getPDO());
 
-	//create a new guest object
-	echo "Creating new Address object...";
-	$guest = new Guest(1, 2);
-	echo $guest;
-
-	$guest->setEmailId(10);
-	echo $guest;
-
-	$guest->setGuestId(20);
-	echo $guest;
-
-	echo "Success!";
-	return;
-	//insert $address into the database!
-	/*
-	$address->insert($pdoConnection);
-	echo $address;
-
-	//user hides their address and adds a new one (simulate edit address)
-	$address->userDelete($pdoConnection);
-	echo $address;
-	$address->userDelete($pdoConnection);
-	echo $address;
-	*/
-	} catch(PDOException $pdoException) {
-	var_dump($pdoException);
-	echo "Exception: " . $pdoException->getMessage();
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoGuest = Guest::getGuestIdByEmailId($this->getPDO(), $guest->getEmailId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("guest"));
+		$this->assertSame($pdoGuest->getGuestId(), $guest->getGuestId());
+	}
 }
+?>
