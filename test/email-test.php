465bb4da7c2099b84bb26c0f93a7d023090157dd
Kyla Carroll <kylacarroll43@gmail.com>
2015-05-14 15:53:42 -0600

    beginning to run unit test on email class
    https://app.asana.com/0/31506975298069/33387218874584

--- test/email-test.php
+++ test/email-test.php
@@ -1,43 +1,223 @@
 <?php
-//requires the AES 256 functions
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+// grab the project test parameters
+require_once("cheqouttest.php");
 
-//load the account class
-require_once(dirname(__DIR__) . '/php/class/email.php');
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/php/class/email.php");
 
-try {
-	//read and decrypt the configuration array
-	$config = readConfig("/etc/apache2/capstone-mysql/cheqout.ini");
+/**
+ * Full PHPUnit test for the Email class
+ *
+ * This is a complete PHPUnit test of the Address class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Email
+ * @author Kyla Carroll <kylacarroll43@gmail.com>
+ **/
+class EmailTest extends CheqoutTest {
+	/**
+	 * valid email address
+	 * @var string $VALID_EMAILADDRESS
+	 **/
+	protected $VALID_EMAILADDRESS = "giblets@turkey.com";
+	/**
+	 * valid stripeId to use
+	 * @var string $VALID_STRIPEID
+	 **/
+	protected $VALID_STRIPEID = "abcdefghijklmnopqrstuvwxy";
+	/**
+	 * valid email address 2
+	 * @var string $VALID_EMAILADDRESS2
+	 */
+	protected $VALID_EMAILADDRESS2 = "innards@chicken.com";
 
-	//create a data connection string(dsn) and specify the login credentials
-	$dsn = "mysql:host=" . $config["hostname"] . ";dbname=" . $config["database"];
+	/**
+	 * test insert on valid email and verify mySQL data matches
+	 **/
+	public function testInsertValidEmail() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
 
-	//enable UTF-8 text handling
-	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+		// create a new Address and insert to into mySQL
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->insert($this->getPDO());
 
-	//connect to mySQL via PDO
-	$pdo = new PDO($dsn, $config["username"], $config["password"], $options);
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoEmail = Email::getEmailByStripeId($this->getPDO(), $email->getEmailId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("email"));
+		$this->assertSame($pdoEmail->getEmailAddress(), $this->VALID_EMAIL);
+		$this->assertSame($pdoEmail->getStripeId(), $this->VALID_STRIPEID);
+	}
+	/**
+	 * test insert on an invalid email
+	 *
+	 * @expectedException PDOException faulty insert function throws this exception
+	 **/
+	public function testInsertInvalidEmail() {
+		$email = new Email(CheqoutTest::INVALID_KEY, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->insert($this->getPDO());
+	}
 
-	//have PDO throw exceptions when possible
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	/**
+	 * test delete of valid email
+	 **/
+	public function testDeleteValidEmail() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
 
-	//insert new email address into sql
-	$email = new email (null, "its@yu.com", "ermagerdstripeid");
-	$email->insert($pdo);
+		// create a new email and insert to into mySQL
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
 
-	//update an email address in mysql
-	$email->setEmailAddress("diemf@dmfd.com");
-	$email->setStripeId("the coolest customer");
-	$email->update($pdo);
+		$email->insert($this->getPDO());
 
-	//select an email
-	$pdoName = Email::getEmailByEmailAddress($pdo, $email->getEmailAddress());
+		//run the delete function
+		$email->delete($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoEmail = Email::getEmailByStripeId($this->getPDO(), $email->getEmailId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("email"));
+		$this->assertSame($pdoEmail->getStripeId(), $this->VALID_STRIPEID);
+	}
 
-	//delete the email from mySQL and show that it is gone
-	$email->delete($pdo);
-	$pdoName = Email::getEmailByEmailAddress($pdo, $email->getEmailAddress());
+	/**
+	 * test delete of invalid email
+	 *
+	 * @ExpectedException PDOException faulty delete function throws this exception
+	 */
+	public function testDeleteInvalidEmail() {
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->delete($this->getPDO());
+	}
+	/**
+	 * test update on a valid email
+	 **/
+	public function testUpdateValidEmail() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
 
-} catch (PDOException $pdoException) {
-	echo "Exception: " . $pdoException->getMessage();
+		// create a new Address and insert to into mySQL
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->insert($this->getPDO());
+
+		// update the email address field
+		$email->setEmailAddress($this->VALID_EMAILADDRESS2);
+		$email->update($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoEmail = Email::getEmailByEmailId($this->getPDO(), $email->getEmailId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("email"));
+		$this->assertSame($pdoEmail->getEmailAddress, $this->VALID_EMAILADDRESS2);
+	}
+	/**
+	 * test update with invalid email address
+	 *
+	 * @ExpectedException PDOException
+	 */
+	public function testUpdateInvalidEmail() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
+
+		// create a new email and insert to into mySQL
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->insert($this->getPDO());
+
+		//update the email address field
+		$email->setEmailAddress(CheqoutTest::INVALID_STRING);
+		$email->update($this->getPDO());
+	}
+
+	/**
+	 * test getting email by email id
+	 **/
+	public function testGetValidEmailByEmailId() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
+		// create a new address and insert to into mySQL
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Email::getEmailByEmailId($this->getPDO(), $email->getEmailId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("email"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Email", $results);
+		// grab the result from the array and validate it
+		$pdoEmail = $results[0];
+		$this->assertSame($pdoEmail->getEmailAddress(), $this->VALID_EMAILADDRESS);
+		$this->assertSame($pdoEmail->getStripeId(), $this->VALID_STRIPEID);
+	}
+	/**
+	 * test grabbing an email that does not exist by email id
+	 **/
+	public function testGetInvalidEmailByEmailId() {
+		// grab an email id that exceeds the maximum allowable address id
+		$email = Email::getEmailByEmailId($this->getPDO(), CheqoutTest::INVALID_KEY);
+		$this->assertNull($email);
+	}
+	/**
+	 * test inserting an email and re-grabbing it from mySQL by stripeId
+	 **/
+	public function testGetValidEmailByStripeId() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
+		// create a new address and insert to into mySQL
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Email::getEmailByStripeId($this->getPDO(), $email->getEmailId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("email"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Email", $results);
+		// grab the result from the array and validate it
+		$pdoEmail = $results[0];
+		$this->assertSame($pdoEmail->getEmailAddress(), $this->VALID_EMAILADDRESS);
+		$this->assertSame($pdoEmail->getStripeId(), $this->VALID_STRIPEID);
+	}
+	/**
+	 * test grabbing an email that does not exist by stripe id
+	 **/
+	public function testGetInvalidEmailByStripeId() {
+		// grab an email id that exceeds the maximum allowable address id
+		$email = Email::getEmailByStripeId($this->getPDO(), CheqoutTest::INVALID_STRING);
+		$this->assertNull($email);
+	}
+	/**
+	 * test getting all emails and verifying same as inserted
+	 */
+	public function testGetAllEmails() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
+		// create a new address and insert to into mySQL
+		$email = new Email(null, $this->VALID_EMAILADDRESS, $this->VALID_STRIPEID);
+		$email->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Email::getAllEmails($this->getPDO(), $email->getEmailId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("email"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Email", $results);
+		// grab the result from the array and validate it
+		$pdoEmail = $results[0];
+		$this->assertSame($pdoEmail->getEmailAddress(), $this->VALID_EMAILADDRESS);
+		$this->assertSame($pdoEmail->getStripeId(), $this->VALID_STRIPEID);
+	}
+
+	/**
+	 * test breaking email address string
+	 *
+	 * @expectedException UnexpectedValueException
+	 **/
+
+	public function testBreakingEmailAddressString() {
+		//make a new email entry and break email address
+		new Email(null, CheqoutTest::INVALID_STRING, $this->VALID_STRIPEID);
+	}
+	/**
+	 * test breaking stripe id string
+	 *
+	 * @expectedException UnexpectedValueException
+	 **/
+
+	public function testBreakingStripeIdString() {
+		//make a new email entry and break stripe id string
+		new Email(null, $this->VALID_EMAILADDRESS, CheqoutTest::INVALID_STRING);
+	}
+
 }
-?>
