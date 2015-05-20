ef4dad7d4191b0dcc83f3ca4612170c2bbd8da6a
James Hill <james.w.l.hill@gmail.com>
2015-05-20 15:05:18 -0600

    wrote json controller and search/index.php for site search functionality https://app.asana.com/0/31506975298102/31506975298102

---
+++ controllers/jsoncontroller.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Format spl fixed array of products in JSON
+ */
+
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once(dirname(dirname(__DIR__)) . "/php/class/product.php");
+
+/**
+ * sets up the database connection
+ */
+$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cheqout.ini");
+
+/**
+ * Make an array of products from MySQL to pass to json_encode for use in typeahead plugin
+ */
+$userSearch = getProductByProductDescription();
+
+/**
+ * Encode array of products into json format and store in fs
+ */
+$json = json_encode($userSearch);
+
+?>
---
+++ search/index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Cheqout search index
+ *
+ * @author James Hill <james@appists.com>
+ */
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head lang="en">
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>Cheqout autocomplete site search</title>
+
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css" />
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		//user input search bar
+		<div class="container">
+			<p>Search Products</p>
+			<input id="my-input" class="typeahead" type="text" placeholder="search for a product">
+		</div>
+		<!-- Load jQuery and the typeahead JS files -->
+		<script src="//code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.9.3/typeahead.min.js"></script>
+
+		<script type="text/javascript">
+			$(function(){
+				// apply typeahead to the text input box
+				$('#my-input').typeahead({
+					name: 'products',
+
+					// data source
+					prefetch: '../controllers/jsoncontroller.php',
+
+					// max items in the dropdown
+					limit: 5
+				});
+
+			});
+		</script>
+	</body>
+</html>
