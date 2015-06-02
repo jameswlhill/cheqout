7a30dff6d23a2b781c79dfa880cd3c28a225d783
Kyla Carroll <kylacarroll43@gmail.com>
2015-06-02 13:40:59 -0600

    registration integration

--- register/index.php
+++ register/index.php
@@ -19,15 +19,12 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 		<h2>Register with Cheqout</h2>
 		<form id='register' action='register.php' role="form" method='post' accept-charset='UTF-8'>
 			<div class="form-group">
-				<label for='email'></label>
 				<input type='text' name='email' id='email' maxlength="128" placeholder="your@email.here"/>
 			</div>
 			<div class="form-group">
-				<label for='password'></label>
 				<input type='password' name='password' id='password' maxlength="128" placeholder="Password" />
 			</div>
 			<div class="form-group">
-				<label for='password2'></label>
 				<input type='password' name='password2' id='password2' maxlength="128" placeholder="Retype Password"/>
 			</div>
 				<input type='submit' name='Submit' value='Submit' class="btn btn-primary" />
