45e9e5e3c6d1048f40c6e9ed734328831d233ab8
Tyler Wiegand <tyler.wiegand@me.com>
2015-06-03 08:44:45 -0600

    first attempt at new method of displaying orders
    https://app.asana.com/0/31506975298080/34448909122781

--- account/ordersgetcontroller.php
+++ account/ordersgetcontroller.php
@@ -23,50 +23,28 @@ try {
 	$orderArray = Email::getOrdersByEmail($pdo, $email->getEmailId());
 	if(is_array($orderArray[0]) === true) {
 		try {
-			echo '<div class="row">';
-			echo '<div class="col-md-6 pull-left">';
-			echo '<div class="text-info">Shipping Address</p>';
-			echo $orderArray[0][9] . '</p><p>' . //label
-				$orderArray[0][8] . '</p><p>' . //attention
-				$orderArray[0][10] . '</p><p>' . //street1
-				$orderArray[0][11] . '</p><p>' . //street2
-				$orderArray[0][12] . '</p><p>' . //city
-				$orderArray[0][13] . '</p><p>' . //state
-				$orderArray[0][14] . '</p><p>';  //zip
-			echo '<p class="text-info">Order</p>';
-			echo $orderArray[0][1] . '</p><p>' .  //order id
-				$orderArray[0][15] . '</p><p>'; //order date/time
-			echo '<p class="text-info">Order Total</p>';
-			echo $orderArray[0][7] . '</p><p>'; //order total
-			echo '</div>';
 			$tracker = 0;
 			$tracker2 = 1;
 			foreach($orderArray as $list) {
 				if($orderArray[$tracker][1] !== $orderArray[$tracker2][1]) {
-					echo '<div class="row">';
-					echo '<div class="col-md-6">';
-					echo '<p class="text-info">Shipping Address</p>';
-					echo $list[9] . '</p><p>' . //label
-						$list[8] . '</p><p>' . //attention
-						$list[10] . '</p><p>' . //street1
-						$list[11] . '</p><p>' . //street2
-						$list[12] . '</p><p>' . //city
-						$list[13] . '</p><p>' . //state
-						$list[14] . '</p><p>';  //zip
-					echo '<p class="text-info">Order</p>';
-					echo $list[1] . '</p><p>' .  //order id
-						$list[15] . '</p><p>'; //order date/time
-					echo '<p class="text-info">Order Total</p>';
-					echo $list[7] . '</p><p>'; //order total
-					echo '</div>';
-				}
-				if($orderArray[$tracker][1] === $orderArray[$tracker2][1]) {
-					echo '<div class="col-md-6">';
-					echo '<p class="text-info">Product</p>';
-					echo $list[4] . '</p><p>' .   //product name
-						$list[2] . '</p><p>' .   //quantity
-						$list[5] . '</p><p>';   //price total
-					echo '</div>';
+					echo	'<div class="container">';
+					echo		'<div class="row">';
+					echo 			'<div class="col-md-3 text-info">Date of Purchase</div>';
+					echo 			'<div class="col-md-3 text-info">Order Total</div>';
+					echo 			'<div class="col-md-3 text-info">Shipped To</div>';
+					echo 			'<div class="col-md-3 text-info">Order Number</div>';
+					echo		'</div>';
+					echo		'<div class="row">';
+					echo 			'<div class="col-md-3">' . $list[15] . '</div>';			//purchase datetime
+					echo 			'<div class="col-md-3">' . $list[7] . '</div>';				//order total
+					echo 			'<div class="col-md-3">';											//if we have a label, Label@Name
+										if(@isset($list[9]) === true && $list[9] !== null) {	//if not, just the name
+											echo $list[8] . '@' . $list[9];
+										} else echo $list[8];
+					echo			'</div>';
+					echo 			'<div class="col-md-3">' . $list[1] . '</div>';				//finally, the order number
+					echo		'</div>';
+					echo	'</div>';
 				}
 				$tracker++;
 				$tracker2++;
@@ -77,6 +55,53 @@ try {
 		}
 	}
 
+
+/*
+*  The graveyard. Where code comes to die.
+*/
+
+//				if($orderArray[$tracker][1] !== $orderArray[$tracker2][1]) {
+//					echo '<div class="row">';
+//					echo '<div class="col-md-6">';
+//					echo '<p class="text-info">Shipping Address</p>';
+//					echo $list[9] . '</p><p>' . //label
+//						$list[8] . '</p><p>' . //attention
+//						$list[10] . '</p><p>' . //street1
+//						$list[11] . '</p><p>' . //street2
+//						$list[12] . '</p><p>' . //city
+//						$list[13] . '</p><p>' . //state
+//						$list[14] . '</p><p>';  //zip
+//					echo '<p class="text-info">Order</p>';
+//					echo $list[1] . '</p><p>' .  //order id
+//						$list[15] . '</p><p>'; //order date/time
+//					echo '<p class="text-info">Order Total</p>';
+//					echo $list[7] . '</p><p>'; //order total
+//					echo '</div>';
+//				}
+
+//			echo '<div class="row">';
+//			echo '<div class="col-md-6 pull-left">';
+//			echo '<div class="text-info">Shipping Address</p>';
+//			echo $orderArray[0][9] . '</p><p>' . //label
+//				$orderArray[0][8] . '</p><p>' . //attention
+//				$orderArray[0][10] . '</p><p>' . //street1
+//				$orderArray[0][11] . '</p><p>' . //street2
+//				$orderArray[0][12] . '</p><p>' . //city
+//				$orderArray[0][13] . '</p><p>' . //state
+//				$orderArray[0][14] . '</p><p>';  //zip
+//			echo '<p class="text-info">Order</p>';
+//			echo $orderArray[0][1] . '</p><p>' .  //order id
+//				$orderArray[0][15] . '</p><p>'; //order date/time
+//			echo '<p class="text-info">Order Total</p>';
+//			echo $orderArray[0][7] . '</p><p>'; //order total
+//			echo '</div>';
+
+
+//				echo '<p class="text-info">Product</p>';
+//				echo $list[4] . '</p><p>' .   //product name
+//					$list[2] . '</p><p>' .   //quantity
+//					$list[5] . '</p><p>';   //price total
+//				echo '</div>';
 
 //	<th>Email Address</th>0
 //<th>Order ID</th>1
