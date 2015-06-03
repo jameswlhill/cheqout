d64586dde03feeddc5cafcb302706266ef09b2d4
Kyla Carroll <kylacarroll43@gmail.com>
2015-06-03 11:03:26 -0600

    email change email

--- account/emailchange.php
+++ account/emailchange.php
@@ -5,6 +5,7 @@ require_once("Mail.php");
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
+$PAGE_TITLE = "Change Email - Cheqout";
 // go into the database and grab their email object
 $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cheqout.ini");
 if(@isset($_SESSION["email"])) {
@@ -13,24 +14,32 @@ if(@isset($_SESSION["email"])) {
 if(@isset($_SESSION["account"])) {
 	$account = $_SESSION["account"];
 }
-
-try {
-	if(@isset($_POST["newemail"]) === false  ||
-		(@isset($_POST["emailcheck"]) === false) ||
-		$_POST["newemail"] !== $_POST["emailcheck"]) {
-		throw(new InvalidArgumentException("Please fill in all required fields."));
+if(isset($_POST["submit"])) {
+	try {
+		if(@isset($_POST["newemail"]) === false ||
+			(@isset($_POST["emailcheck"]) === false) ||
+			$_POST["newemail"] !== $_POST["emailcheck"]
+		) {
+			throw(new InvalidArgumentException("Please fill in all required fields and make sure they match."));
+		}
+		$email = new Email($_SESSION["email"]->getEmailId(), $_POST["emailcheck"], $_SESSION["email"]->getStripeId());
+		$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cheqout.ini");
+		$email->update($pdo);
+		echo "<p class=\"alert alert-success\">E-Mail (id = " . $email->getEmailId() . ") posted!</p>";
+	} catch(Exception $exception) {
+		echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
 	}
-	$email = new Email($_SESSION["email"]->getEmailId(),$_POST["emailcheck"] ,$_SESSION["email"]->getStripeId());
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cheqout.ini");
-	$email->update($pdo);
-	echo "<p class=\"alert alert-success\">E-Mail (id = " . $email->getEmailId() . ") posted!</p>";
-}	catch(Exception $exception) {
-	echo "<p class=\"alert alert-danger\">Exception: " . $exception->getMessage() . "</p>";
 }
-
-
 ?>
 
-<form class="form-inline">
+<section class="side-panel col-md-3">
+	<?php require_once("../lib/sidebar.php"); ?>
+</section>
+<header>
+	<?php require_once("../lib/header.php"); ?>
+</header>
 
-</form>
+<div class="container-fluid">
+	<h2>Change your email</h2>
+<?php require_once("emailchangeform.php"); ?>
+</div>
---
+++ account/emailchangeform.php
@@ -0,0 +1,7 @@
+<div class="container"></div>
+<form class="form-inline" id="emailchange" method="post" action="emailchange.php">
+	<input type="email" class="form-control" id="newemail" name="newemail" placeholder="your@new.email">
+	<input type="email" class="form-control" id="emailcheck" name="emailcheck" placeholder="Retype Email">
+	<button type="submit" class="btn btn-primary">Change Email</button>
+</form>
+</div>
